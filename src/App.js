9417a1616b41f42da35bbc2b38a4518cfe6a43bd
saber ayoub smatti <ayooubsmatti@gmail.com>
2022-11-06 20:11:17 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import { Header,Container,Footer } from './Components/index'
+
+const App = ()=>{
+    return(
+        <>
+            <Header />
+            <Container>content</Container>
+            <Footer />
+            
+        </>
+    )
+}
+
+export default App
---
+++ src/Components/Container/Container.js
@@ -0,0 +1,11 @@
+import './Container.css'
+
+const Container = (props)=>{
+    return(
+        <>
+            <h1>{props.children}</h1>
+        </>
+    )
+}
+
+export default Container
